f0f1c2fce2dba7fd5233268ee09129bb1f31ff40
KyeongMinBae <reaaaaaaal@gmail.com>
2025-10-07 21:22:23 +0900

    패키지 - 시작

--- src/pack/PackageMain1.java
+++ src/pack/PackageMain1.java
@@ -1,11 +1,9 @@
 package pack;
 
-import pack.a.User;
-
 public class PackageMain1 {
 
     public static void main(String[] args) {
         Data data = new Data();
-        User user = new User();
+        pack.a.User user = new pack.a.User();
     }
 }
