2036223ab033733f766d59452df7c123a25785e3
KyeongMinBae <reaaaaaaal@gmail.com>
2025-10-19 21:44:10 +0900

    super - 생성자

---
+++ src/extends1/super2/Super2Main.java
@@ -0,0 +1,9 @@
+package extends1.super2;
+
+public class Super2Main {
+
+    public static void main(String[] args) {
+        ClassC classC = new ClassC();
+        System.out.println("ClassC 생성자");
+    }
+}
